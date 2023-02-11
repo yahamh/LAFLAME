a6e2b94f8f3352707d9182cda9d586ae5cd5560f
Yacine <yacine.hamdani@polymtl.ca>
2023-02-10 20:12:15 -0500

    yacine aime les bites

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yahamh/LAFLAME.git 3dModel:gh-pages
+git push -f git@github.com:yahamh/LAFLAME.git main:3dModel
 
 cd -
